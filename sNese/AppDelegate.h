8ed90af581c93e24156b9dd129e68e320a34cd47
Neil <Neil@Neils-iMac.local>
2011-12-07 19:02:48 -0600

    Initial Commit

---
+++ sNese/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sNese
+//
+//  Created by Neil Singh on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
